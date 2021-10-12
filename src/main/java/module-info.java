8edffb5cf7900e337ad1f71a2013dd078e24aeb0
TaDinhDucHieu <tadinhduchieu2709@gmail.com>
2021-10-12 15:15:33 +0700

    Add text-to-speech method in HelloController.java. Add freetts jar file in resource.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example._mydictionary {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jsapi;
+    requires freetts;
 
 
     opens com.example._mydictionary to javafx.fxml;
